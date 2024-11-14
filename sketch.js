e515f24eb1148d910b76c19847a3a72294ee9eb4
etha7881 <etha7881@colorado.edu>
2024-11-14 14:38:10 -0700

    Version One

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let StandardmapButton;
+let mapcolor;
+let mapcolorsize;
+let mapcolorgradient;
+let mapterrain;
+let mapstandard;
+
+function preload() {
+	mapcolor=loadImage('Map Colored.png')
+	
+	}
+function setup() 
+{
+	createCanvas(1920, 1080);
+	mapColor();
+
+}
+
+function createButtons(){
+StandardmapButton = createButton("Standard Map");
+
+StandardButton.position(20, 1940);
+
+}
+function mapColor(){
+
+image(mapcolor, 0, 0);
+
+}
+function draw(){
+	createButtons();
+}
+	
