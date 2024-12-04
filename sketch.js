71cbb421116f2f4e1d2c44fdb435f43ee4dde463
etha7881 <etha7881@colorado.edu>
2024-12-04 12:58:09 -0700

    ADDED fire buttons

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup()
 {
 	createCanvas(1920, 1080);
 	mapColor();
-
+	drawFire(100,200, 100);
 }
 
 function createButtons(){
@@ -29,6 +29,35 @@ image(mapcolor, 0, 0);
 }
 function draw(){
 	createButtons();
+	
 }
+function drawFire(x, y, size) {
+	noStroke();
+  
+	// Draw the outer flame (red)
+	fill(255, 69, 0); // Red-orange color
+	beginShape();
+	vertex(x, y);
+	bezierVertex(x - size, y - size, x - size * 0.3, y - size * 1.5, x, y - size * 1.8);
+	bezierVertex(x + size * 0.3, y - size * 1.5, x + size, y - size, x, y);
+	endShape(CLOSE);
+  
+	// Draw the inner flame (orange)
+	fill(255, 140, 0); // Orange color
+	beginShape();
+	vertex(x, y);
+	bezierVertex(x - size * 0.6, y - size * 0.6, x - size * 0.2, y - size * 1.2, x, y - size * 1.4);
+	bezierVertex(x + size * 0.2, y - size * 1.2, x + size * 0.6, y - size * 0.6, x, y);
+	endShape(CLOSE);
+  
+	// Draw the core flame (yellow)
+	fill(255, 215, 0); // Yellow color
+	beginShape();
+	vertex(x, y);
+	bezierVertex(x - size * 0.3, y - size * 0.3, x, y - size * 0.8, x, y - size);
+	bezierVertex(x, y - size * 0.8, x + size * 0.3, y - size * 0.3, x, y);
+	endShape(CLOSE);
+  }
+  
 	
 //hello yooo
