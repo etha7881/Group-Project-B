7bed38fdcd0962239c70933fd72e39f94e3d9a4e
rysh44 <ryashfr@gmail.com>
2024-12-07 15:41:41 -0700

    Fire Labels
    I changed around a good bit and made set names for each fire so that we know which one is which.

--- sketch.js
+++ sketch.js
@@ -1,109 +1,107 @@
-let StandardmapButton,SataliteButton;
-
 let mapcolor;
-let mapcolorsize;
-let mapcolorgradient;
-let mapterrain;
-let mapstandard;
+let StandardButton, SataliteButton;
+
+
+let fire1 = { x: 90, y: 255, size: 40 };
+let fire2 = { x: 145, y: 460, size: 40 };
+let fire3 = { x: 118, y: 680, size: 40 };
+let fire4 = { x: 155, y: 879, size: 40 };
+let fire5 = { x: 283, y: 925, size: 40 };
+let fire6 = { x: 770, y: 920, size: 40 };
+let fire7 = { x: 665, y: 720, size: 40 };
+let fire8 = { x: 788, y: 750, size: 40 };
+let fire9 = { x: 896, y: 650, size: 40 };
+let fire10 = { x: 657, y: 140, size: 40 };
+let fire11 = { x: 588, y: 240, size: 40 };
+let fire12 = { x: 505, y: 235, size: 40 };
+let fire13 = { x: 388, y: 405, size: 40 };
+let fire14 = { x: 278, y: 100, size: 40 };
+let fire15 = { x: 235, y: 120, size: 40 };
+let fire16 = { x: 479, y: 130, size: 40 };
+let fire17 = { x: 520, y: 55, size: 40 };
+let fire18 = { x: 485, y: 50, size: 40 };
+let fire19 = { x: 1243, y: 94, size: 40 };
+let fire20 = { x: 1350, y: 935, size: 40 };
+let fire21 = { x: 1200, y: 50, size: 40 };
 
 function preload() {
-	mapcolor=loadImage('Map Colored.png')
-	
-	}
-function setup() 
-{
-	createCanvas(1920, 1080);
-	
-	mapColor();
-	drawFire(90,255, 40);
-	drawFire(145,460, 40);
-	drawFire(118,680, 40);
-	drawFire(155,879, 40);
-	drawFire(283,925, 40);
-	drawFire(770,920, 40);
-	drawFire(665,720, 40);
-	drawFire(788,750, 40);
-	drawFire(896,650, 40);
-	drawFire(657,140, 40);
-	drawFire(588,240, 40);
-	drawFire(505,235, 40);
-	drawFire(388,405, 40);
-	drawFire(278,100, 40);
-	drawFire(235,120, 40);
-	drawFire(479,130, 40);
-	drawFire(520,55, 40);
-	drawFire(485,50, 40);
-	drawFire(1243,94, 40);
-	drawFire(1350,935, 40);
+    mapcolor = loadImage('Map Colored.png')
 }
 
-function createButtons(){
-	const buttonWidth = 100;
-  const buttonHeight = 35;
-  const buttonMargin = 5;
-
-  StandardButton = createButton('Standard');
-  StandardButton.size(buttonWidth, buttonHeight);
-  StandardButton.position(5,1090);
-
-  SataliteButton = createButton('Satalite');
-  SataliteButton.size(buttonWidth, buttonHeight);
-  SataliteButton.position(110,1090);
-
-
-
-
-
-
-
+function setup() {
+    createCanvas(1920, 1080);
+    mapColor();
+    drawAllFires();
+    createButtons();
 }
-function mousePressed() {
-	// Check for shape selection buttons
-	if (dist(mouseX, mouseY, width - 180, 180) < 20) {
-	  selectedShape = 'circle'; // Change selected shape to circle
-	} else if (mouseX > width - 165 && mouseX < width - 145 && mouseY > 170 && mouseY < 190) {
-	  selectedShape = 'triangle'; // Change selected shape to triangle
-	} else if (mouseX > width - 138 && mouseX < width - 118 && mouseY > 170 && mouseY < 190) {
-	  selectedShape = 'square'; // Change selected shape to square
-	}
-}
-function mapColor(){
 
-image(mapcolor, 0, 0);
-
+function mapColor() {
+    image(mapcolor, 0, 0);
 }
+
 function drawFire(x, y, size) {
-	noStroke();
-  
-	// Draw the outer flame (red)
-	fill(255, 69, 0); // Red-orange color
-	beginShape();
-	vertex(x, y);
-	bezierVertex(x - size, y - size, x - size * 0.3, y - size * 1.5, x, y - size * 1.8);
-	bezierVertex(x + size * 0.3, y - size * 1.5, x + size, y - size, x, y);
-	endShape(CLOSE);
-  
-	// Draw the inner flame (orange)
-	fill(255, 140, 0); // Orange color
-	beginShape();
-	vertex(x, y);
-	bezierVertex(x - size * 0.6, y - size * 0.6, x - size * 0.2, y - size * 1.2, x, y - size * 1.4);
-	bezierVertex(x + size * 0.2, y - size * 1.2, x + size * 0.6, y - size * 0.6, x, y);
-	endShape(CLOSE);
-  
-	// Draw the core flame (yellow)
-	fill(255, 215, 0); // Yellow color
-	beginShape();
-	vertex(x, y);
-	bezierVertex(x - size * 0.3, y - size * 0.3, x, y - size * 0.8, x, y - size);
-	bezierVertex(x, y - size * 0.8, x + size * 0.3, y - size * 0.3, x, y);
-	endShape(CLOSE);
-  }
-function draw(){
-	createButtons();
+    noStroke();
 
+    // Outer flame
+    fill(255, 69, 0);
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size, y - size, x - size * 0.3, y - size * 1.5, x, y - size * 1.8);
+    bezierVertex(x + size * 0.3, y - size * 1.5, x + size, y - size, x, y);
+    endShape(CLOSE);
+
+    // Inner flame
+    fill(255, 140, 0);
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size * 0.6, y - size * 0.6, x - size * 0.2, y - size * 1.2, x, y - size * 1.4);
+    bezierVertex(x + size * 0.2, y - size * 1.2, x + size * 0.6, y - size * 0.6, x, y);
+    endShape(CLOSE);
+
+    // Core flame
+    fill(255, 215, 0);
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size * 0.3, y - size * 0.3, x, y - size * 0.8, x, y - size);
+    bezierVertex(x, y - size * 0.8, x + size * 0.3, y - size * 0.3, x, y);
+    endShape(CLOSE);
 }
 
-  
-	
-//hello yooo
+function drawAllFires() {
+    
+    drawFire(fire1.x, fire1.y, fire1.size);
+    drawFire(fire2.x, fire2.y, fire2.size);
+    drawFire(fire3.x, fire3.y, fire3.size);
+    drawFire(fire4.x, fire4.y, fire4.size);
+    drawFire(fire5.x, fire5.y, fire5.size);
+    drawFire(fire6.x, fire6.y, fire6.size);
+    drawFire(fire7.x, fire7.y, fire7.size);
+    drawFire(fire8.x, fire8.y, fire8.size);
+    drawFire(fire9.x, fire9.y, fire9.size);
+    drawFire(fire10.x, fire10.y, fire10.size);
+    drawFire(fire11.x, fire11.y, fire11.size);
+    drawFire(fire12.x, fire12.y, fire12.size);
+    drawFire(fire13.x, fire13.y, fire13.size);
+    drawFire(fire14.x, fire14.y, fire14.size);
+    drawFire(fire15.x, fire15.y, fire15.size);
+    drawFire(fire16.x, fire16.y, fire16.size);
+    drawFire(fire17.x, fire17.y, fire17.size);
+    drawFire(fire18.x, fire18.y, fire18.size);
+    drawFire(fire19.x, fire19.y, fire19.size);
+    drawFire(fire20.x, fire20.y, fire20.size);
+    drawFire(fire21.x, fire21.y, fire21.size);
+}
+
+function createButtons() {
+    const buttonWidth = 100;
+    const buttonHeight = 35;
+
+    StandardButton = createButton('Standard');
+    StandardButton.size(buttonWidth, buttonHeight);
+    StandardButton.position(5, height - 50);
+
+    SataliteButton = createButton('Satalite');
+    SataliteButton.size(buttonWidth, buttonHeight);
+    SataliteButton.position(110, height - 50);
+}
+
